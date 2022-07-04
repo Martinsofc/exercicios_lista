5cce82ae4f07fdbcf84ee0f69b65dcc652f70295
Martinsofc <107128642+Martinsofc@users.noreply.github.com>
2022-07-04 10:52:37 -0300

    Add files via upload

---
+++ mucilon.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+import javax.swing.*;
+
+public class mucilon {
+    
+    public static void main(String[] args){
+       try (Scanner imput = new Scanner(System.in)){
+        int idade;
+          System.out.println( "Digite sua idade:");
+          idade = imput.nextInt();
+
+          if (idade <= 2)
+          {
+            JOptionPane.showMessageDialog(null, "Bebê");
+          }
+
+          if (idade <= 11)
+            {
+                JOptionPane.showMessageDialog(null, "Criança");
+            }
+
+          if (idade <= 19) 
+          {
+            JOptionPane.showMessageDialog(null, "Adolescente");
+          } 
+          if (idade <= 30)
+          {
+            JOptionPane.showMessageDialog(null, "Jovem");
+          }
+
+          if (idade <= 60)
+          {
+            JOptionPane.showMessageDialog(null, "Adulto");
+          }
+
+          if (idade <= 61) 
+          {
+            JOptionPane.showMessageDialog(null, "Idoso");
+          } 
+
+     
+
+       }
+        
+    }
+
+
+
+
+
+}
