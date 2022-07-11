8ef03860998a18f43d361125882dc8cfd53a5f8b
Martinsofc <107128642+Martinsofc@users.noreply.github.com>
2022-07-11 08:24:42 -0300

    Update ImparPar.java

--- ImparPar.java
+++ ImparPar.java
@@ -1,3 +1,4 @@
+//Hiury Martins 
 import java.lang.management.ThreadInfo;
 import java.lang.reflect.Method;
 import java.util.Scanner;
@@ -13,7 +14,7 @@ public class ImparPar{
     System.out.println("Escreva o número.");
     num1 = entrada.nextInt();
 
-if (num1 % 2 == 0) {
+    if (num1 % 2 == 0) {
     System.out.println("O número é par.");
 } else {
 System.out.println("O número é impar.");    
